968e513fcd31498b5ddfccee60375237ba9b684b
Andres <f.martinez5@utp.edu.co>
2022-05-11 17:12:37 -0500

    Se llama la funcion calculadora pero falta iniciar (revisar)

--- 2.1/UI/COMPLEMENTOS/RESISTENCIA/Resistencia_Pirncipal.py
+++ 2.1/UI/COMPLEMENTOS/RESISTENCIA/Resistencia_Pirncipal.py
@@ -1,12 +1,14 @@
 import sys
-from GUI import *
+#from GUI import *
+from UI.COMPLEMENTOS.RESISTENCIA.GUI import *
 class MiApp2(QtWidgets.QMainWindow):
-	def __init__(self):
-		super().__init__()
-		self.ui = Ui_Form2() 
-		self.ui.setupUi2(self)
+	def __init__(self, parent=None):
+		#super().__init__(self,parent)
+		QtWidgets.QWidget.__init__(self,parent)
+		self.ui = Ui_FORM() 
+		self.ui.setupUi(self)
 
-		self.multiplicadorDIC = {
+		self.multiplicadorDIC = { 
 		"Plata":0.01,
 		"Oro":0.1,
 		"Negro":1,
@@ -47,174 +49,174 @@ class MiApp2(QtWidgets.QMainWindow):
 
 		self.ui.bt_resultado.clicked.connect(self.obtener_Resultado)
 
-		self.ui.selec1.addItems(self.colorDIC.keys())
-		self.ui.selec1.currentIndexChanged.connect(self.seleccion1)
-		self.ui.selec1.setCurrentText("Negro")
+		self.ui.select1.addItems(self.colorDIC.keys())
+		self.ui.select1.currentIndexChanged.connect(self.seleccion1)
+		self.ui.select1.setCurrentText("Negro")
 
-		self.ui.selec2.addItems(self.colorDIC.keys())
-		self.ui.selec2.currentIndexChanged.connect(self.seleccion2)
-		self.ui.selec2.setCurrentText("Negro")
+		self.ui.select2.addItems(self.colorDIC.keys())
+		self.ui.select2.currentIndexChanged.connect(self.seleccion2)
+		self.ui.select2.setCurrentText("Negro")
 
-		self.ui.selec3.addItems(self.multiplicadorDIC.keys())
-		self.ui.selec3.currentIndexChanged.connect(self.seleccion3)
-		self.ui.selec3.setCurrentText("Negro")
+		self.ui.select3.addItems(self.multiplicadorDIC.keys())
+		self.ui.select3.currentIndexChanged.connect(self.seleccion3)
+		self.ui.select3.setCurrentText("Negro")
 
-		self.ui.selec4.addItems(self.toleracionDIC.keys())
-		self.ui.selec4.currentIndexChanged.connect(self.seleccion4)
-		self.ui.selec4.setCurrentText("Ninguno")
+		self.ui.select4.addItems(self.toleracionDIC.keys())
+		self.ui.select4.currentIndexChanged.connect(self.seleccion4)
+		self.ui.select4.setCurrentText("Ninguno")
 
 
 	def seleccion1(self):
-		valor1 = self.ui.selec1.currentText()
+		valor1 = self.ui.select1.currentText()
 
 		if valor1=="Negro":
-			self.ui.label1.setStyleSheet("background-color: rgb(0, 0, 0)")
+			self.ui.label_4.setStyleSheet("background-color: rgb(0, 0, 0)")
 			valor1 = 0
 
 		elif valor1 =="Marron":
-			self.ui.label1.setStyleSheet("background-color: rgb(118, 51, 51)")
+			self.ui.label_4.setStyleSheet("background-color: rgb(118, 51, 51)")
 			self.valor1 = 1
 
 		elif valor1 =="Rojo":
-			self.ui.label1.setStyleSheet("background-color: rgb(255, 0, 0)")
+			self.ui.label_4.setStyleSheet("background-color: rgb(255, 0, 0)")
 			self.valor1 = 2
 
 		elif valor1 =="Naranjado":
-			self.ui.label1.setStyleSheet("background-color: rgb(255, 139, 3)")
+			self.ui.label_4.setStyleSheet("background-color: rgb(255, 139, 3)")
 			self.valor1 = 3
 
 		elif valor1 =="Amarillo":
-			self.ui.label1.setStyleSheet("background-color: rgb(251, 244, 11)")
+			self.ui.label_4.setStyleSheet("background-color: rgb(251, 244, 11)")
 			self.valor1 = 4
 
 		elif valor1 =="Verde":
-			self.ui.label1.setStyleSheet("background-color: rgb(0, 255, 0)")
+			self.ui.label_4.setStyleSheet("background-color: rgb(0, 255, 0)")
 			self.valor1 = 5
 
 		elif valor1 =="Azul":
-			self.ui.label1.setStyleSheet("background-color: rgb(0, 0, 255)")
+			self.ui.label_4.setStyleSheet("background-color: rgb(0, 0, 255)")
 			self.valor1 = 6
 
 		elif valor1 =="Violeta":
-			self.ui.label1.setStyleSheet("background-color: rgb(244, 11, 251)")
+			self.ui.label_4.setStyleSheet("background-color: rgb(244, 11, 251)")
 			self.valor1 = 7
 
 		elif valor1 =="Gris":
-			self.ui.label1.setStyleSheet("background-color: rgb(119, 119, 119)")
+			self.ui.label_4.setStyleSheet("background-color: rgb(119, 119, 119)")
 			self.valor1 = 8
 
 		elif valor1 =="Blanco":
-			self.ui.label1.setStyleSheet("background-color: rgb(0, 0, 0)")
+			self.ui.label_4.setStyleSheet("background-color: rgb(0, 0, 0)")
 			self.valor1 = 9
 
 
 	def seleccion2(self):
-		valor2 = self.ui.selec2.currentText()
+		valor2 = self.ui.select2.currentText()
 
 		if valor2=="Negro":
-			self.ui.label2.setStyleSheet("background-color: rgb(0, 0, 0)")
+			self.ui.label_5.setStyleSheet("background-color: rgb(0, 0, 0)")
 			self.valor2 = 0
 
 		elif valor2 =="Marron":
-			self.ui.label2.setStyleSheet("background-color: rgb(118, 51, 51)")
+			self.ui.label_5.setStyleSheet("background-color: rgb(118, 51, 51)")
 			self.valor2 = 1
 
 		elif valor2 =="Rojo":
-			self.ui.label2.setStyleSheet("background-color: rgb(255, 0, 0)")
+			self.ui.label_5.setStyleSheet("background-color: rgb(255, 0, 0)")
 			self.valor2 = 2
 
 		elif valor2 =="Naranjado":
-			self.ui.label2.setStyleSheet("background-color: rgb(255, 139, 3)")
+			self.ui.label_5.setStyleSheet("background-color: rgb(255, 139, 3)")
 			self.valor2 = 3
 
 		elif valor2 =="Amarillo":
-			self.ui.label2.setStyleSheet("background-color: rgb(251, 244, 11)")
+			self.ui.label_5.setStyleSheet("background-color: rgb(251, 244, 11)")
 			self.valor2 = 4
 
 		elif valor2 =="Verde":
-			self.ui.label2.setStyleSheet("background-color: rgb(0, 255, 0)")
+			self.ui.label_5.setStyleSheet("background-color: rgb(0, 255, 0)")
 			self.valor2 = 5
 
 		elif valor2 =="Azul":
-			self.ui.label2.setStyleSheet("background-color: rgb(0, 0, 255)")
+			self.ui.label_5.setStyleSheet("background-color: rgb(0, 0, 255)")
 			self.valor2 = 6
 
 		elif valor2 =="Violeta":
-			self.ui.label2.setStyleSheet("background-color: rgb(244, 11, 251)")
+			self.ui.label_5.setStyleSheet("background-color: rgb(244, 11, 251)")
 			self.valor2 = 7
 
 		elif valor2 =="Gris":
-			self.ui.label2.setStyleSheet("background-color: rgb(119, 119, 119)")
+			self.ui.label_5.setStyleSheet("background-color: rgb(119, 119, 119)")
 			self.valor2 = 8
 
 		elif valor2 =="Blanco":
-			self.ui.label2.setStyleSheet("background-color: rgb(0, 0, 0)")
+			self.ui.label_5.setStyleSheet("background-color: rgb(0, 0, 0)")
 			self.valor2 = 9
 
 	def seleccion3(self):
-		valor3 = self.ui.selec3.currentText()
+		valor3 = self.ui.select3.currentText()
 		
 		if valor3=="Plata":
-			self.ui.label3.setStyleSheet("background-color: rgb(180, 180, 180)")
+			self.ui.label_6.setStyleSheet("background-color: rgb(180, 180, 180)")
 			self.valor3 = 0.01
 
 		elif valor3 =="Oro":
-			self.ui.label3.setStyleSheet("background-color: rgb(180, 130, 35)")
+			self.ui.label_6.setStyleSheet("background-color: rgb(180, 130, 35)")
 			self.valor3 = 0.1
 
 
 		elif valor3=="Negro":
-			self.ui.label3.setStyleSheet("background-color: rgb(0, 0, 0)")
+			self.ui.label_6.setStyleSheet("background-color: rgb(0, 0, 0)")
 			self.valor3 = 1
 
 		elif valor3 =="Marron":
-			self.ui.label3.setStyleSheet("background-color: rgb(118, 51, 51)")
+			self.ui.label_6.setStyleSheet("background-color: rgb(118, 51, 51)")
 			self.valor3 = 10
 
 		elif valor3 =="Rojo":
-			self.ui.label3.setStyleSheet("background-color: rgb(255, 0, 0)")
+			self.ui.label_6.setStyleSheet("background-color: rgb(255, 0, 0)")
 			self.valor3 = 100
 
 		elif valor3 =="Naranjado":
-			self.ui.label3.setStyleSheet("background-color: rgb(255, 139, 3)")
+			self.ui.label_6.setStyleSheet("background-color: rgb(255, 139, 3)")
 			self.valor3 = 1000
 
 		elif valor3 =="Amarillo":
-			self.ui.label3.setStyleSheet("background-color: rgb(251, 244, 11)")
+			self.ui.label_6.setStyleSheet("background-color: rgb(251, 244, 11)")
 			self.valor3 = 10000
 
 		elif valor3 =="Verde":
-			self.ui.label3.setStyleSheet("background-color: rgb(0, 255, 0)")
+			self.ui.label_6.setStyleSheet("background-color: rgb(0, 255, 0)")
 			self.valor3 = 100000
 
 		elif valor3 =="Azul":
-			self.ui.label3.setStyleSheet("background-color: rgb(0, 0, 255)")
+			self.ui.label_6.setStyleSheet("background-color: rgb(0, 0, 255)")
 			self.valor3 = 1000000
 
 		elif valor3 =="Violeta":
-			self.ui.label3.setStyleSheet("background-color: rgb(244, 11, 251)")
+			self.ui.label_6.setStyleSheet("background-color: rgb(244, 11, 251)")
 			self.valor3 = 10000000
 
 	def seleccion4(self):
-		valor4 = self.ui.selec4.currentText()
+		valor4 = self.ui.select4.currentText()
 		
 		if valor4=="Ninguna":
-			self.ui.label4.setStyleSheet("background-color: rgb(245, 230, 255)")
+			self.ui.label_7.setStyleSheet("background-color: rgb(245, 230, 255)")
 			self.valor4 = 0.2
 			self.Tol = "20%"
 
 		elif valor4 =="Plata":
-			self.ui.label4.setStyleSheet("background-color: rgb(180, 180, 180)")
+			self.ui.label_7.setStyleSheet("background-color: rgb(180, 180, 180)")
 			self.valor4 = 0.1
 			self.Tol = "10%"
 
 		elif valor4 =="Oro":
-			self.ui.label4.setStyleSheet("background-color: rgb(188, 130, 35)")
+			self.ui.label_7.setStyleSheet("background-color: rgb(188, 130, 35)")
 			self.valor4 = 0.05
 			self.Tol = "5%"
 
 		elif valor4 =="Rojo":
-			self.ui.label4.setStyleSheet("background-color: rgb(255, 0, 0)")
+			self.ui.label_7.setStyleSheet("background-color: rgb(255, 0, 0)")
 			self.valor4 = 0.02
 			self.Tol = "2%"
 
@@ -233,7 +235,7 @@ class MiApp2(QtWidgets.QMainWindow):
 		VmaxF = float("{0:.3f}".format(Vmax))
 		VminF = float("{0:.3f}".format(Vmin))
 
-		self.ui.resultado.setText(str(ValorTT))
+		self.ui.label_12.setText(str(ValorTT))
 		self.ui.tolerancia.setText(str(self.Tol))
 		self.ui.maximo.setText(str(VmaxF))
 		self.ui.minimo.setText(str(VminF))
